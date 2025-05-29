988bed75f7b0723cd3dabcf0c83cabb682bad1f7
Payriiz <ibnufarizfauzanhamzah7@gmail.com>
2025-05-29 20:27:39 +0700

    MENAMBAHKAN TUGAS SESI 12

---
+++ ImportModul/aritmatika/math.py
@@ -0,0 +1,11 @@
+def add(num1,num2):
+    return num1 + num2
+
+def multi(num1,num2):
+    return num1 * num2
+
+def pow(num1,num2):
+    return num1 ** num2
+
+def bagi(num1,num2):
+    return num1 / num2
---
+++ ImportModul/ubahbilangan/.ipynb_checkpoints/ubah-checkpoint.py
@@ -0,0 +1,8 @@
+def biner(desimal):
+    return bin(desimal)
+
+def oktal(desimal):
+    return oct(desimal)
+
+def hexa(desimal):
+    return hex(desimal)
